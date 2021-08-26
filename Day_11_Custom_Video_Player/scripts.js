07b3dd472af7fe278ece9ef4624c8ae0849e83b9
trieule50 <trieule50@gmail.com>
2021-08-26 11:14:20 -0400

    Complete day 11

--- Day_11_Custom_Video_Player/scripts.js
+++ Day_11_Custom_Video_Player/scripts.js
@@ -34,15 +34,20 @@ function scrub(e){
     video.currentTime = scrubTime
 }
 
-video.addEventListener('click', togglePlay)
-video.addEventListener('play', updateButton)
-video.addEventListener('pause', updateButton)
-video.addEventListener('timeupdate', handleProgress)
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 
 toggle.addEventListener('click', togglePlay)
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
-progress.addEventListener('click', scrub)
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e)=> mousedown && scrub(e));
+progress.addEventListener('mousedown', ()=> mousedown = true);
+progress.addEventListener('mouseup', ()=> mousedown = false);
