0460db2c65d7d3f9f6dd8e1015bfefb4ee5eefb5
trieule50 <trieule50@gmail.com>
2021-08-26 11:11:11 -0400

    Complete scrub function

--- Day_11_Custom_Video_Player/scripts.js
+++ Day_11_Custom_Video_Player/scripts.js
@@ -29,7 +29,10 @@ function handleProgress(){
     progressBar.style.flexBasis = `${percent}%`
 }
 
-
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime
+}
 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
@@ -41,3 +44,5 @@ toggle.addEventListener('click', togglePlay)
 skipButtons.forEach(button => button.addEventListener('click', skip))
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub)
