e247a295d0f39082efb38b4cf33bddaaded486c1
trieule50 <trieule50@gmail.com>
2021-09-03 11:04:59 -0400

    Update index.html

--- Day_19_Webcam_Fun/scripts.js
+++ Day_19_Webcam_Fun/scripts.js
@@ -30,6 +30,12 @@ function takePhoto(){
     snap.currentTime = 0;
     snap.play();
 
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
 }
 
 getVideo();
