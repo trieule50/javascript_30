1e3ab5acc5fdef614709949e6aea306bd3424092
trieule50 <trieule50@gmail.com>
2021-09-03 11:12:18 -0400

    Add red effect function

--- Day_19_Webcam_Fun/scripts.js
+++ Day_19_Webcam_Fun/scripts.js
@@ -23,6 +23,9 @@ function paintToCanavas(){
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0,0,width, height);
+        pixels = redEffect(pixels);
+        ctx.putImageData(pixels, 0, 0)
     }, 16);
 }
 
@@ -36,6 +39,15 @@ function takePhoto(){
     link.setAttribute('download', 'amazed');
     link.innerHTML = `<img src='${data}' alt='amazed person'>`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+    for(let i=0; i < pixels.data.length; i+=4){
+        pixels[i] = pixels.data[i + 0] + 200;
+        pixels[i+1] = pixels.data[i + 1] - 50;
+        pixels[i+2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
 }
 
 getVideo();
