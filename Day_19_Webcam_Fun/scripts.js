a9bab36796c48cc123895ece4100c094817634a2
trieule50 <trieule50@gmail.com>
2021-09-03 11:06:26 -0400

    Update take photo function

--- Day_19_Webcam_Fun/scripts.js
+++ Day_19_Webcam_Fun/scripts.js
@@ -33,8 +33,8 @@ function takePhoto(){
     const data = canvas.toDataURL('image/jpeg');
     const link = document.createElement('a');
     link.href = data;
-    link.setAttribute('download', 'handsome');
-    link.textContent = 'Download Image';
+    link.setAttribute('download', 'amazed');
+    link.innerHTML = `<img src='${data}' alt='amazed person'>`;
     strip.insertBefore(link, strip.firstChild);
 }
 
