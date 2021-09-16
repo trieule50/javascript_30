32930b1bca531b0e63a1fbb48b846b5d1c6b5ede
trieule50 <trieule50@gmail.com>
2021-09-16 11:11:16 -0400

    Complete display time left

--- Day_29_Countdown_Timer/scripts-START.js
+++ Day_29_Countdown_Timer/scripts-START.js
@@ -1,16 +1,25 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds)
     
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         if(secondsLeft < 0){
             clearInterval(countdown);
             return;
         }
+        displayTimeLeft(secondsLeft)
     }, 1000)
 }
 
 function displayTimeLeft(seconds){
-    
+    const mintues = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${mintues}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds};`
+    document.title = display;
+    timerDisplay.textContent = display;
 }
