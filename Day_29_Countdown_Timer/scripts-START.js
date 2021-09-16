800d3879fd3d17f06580973b07bef50b4c3f9cfa
trieule50 <trieule50@gmail.com>
2021-09-16 11:38:54 -0400

    Complete display end time function

--- Day_29_Countdown_Timer/scripts-START.js
+++ Day_29_Countdown_Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
 
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
-    displayTimeLeft(seconds)
+    displayTimeLeft(seconds);
+    displayEndTime(then);
     
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,3 +26,11 @@ function displayTimeLeft(seconds){
     document.title = display;
     timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const mintues = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${mintues < 10 ? '0' : ''}${mintues}`;
+} 
