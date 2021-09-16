7e3a9a094acaaff6655eb05f7b0182f82c96152d
trieule50 <trieule50@gmail.com>
2021-09-16 11:03:41 -0400

    Add timer function

---
+++ Day_29_Countdown_Timer/scripts-START.js
@@ -0,0 +1,12 @@
+function timer(seconds){
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    
+    setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+    }, 1000)
+}
